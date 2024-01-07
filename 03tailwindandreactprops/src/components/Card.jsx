6fffd736d22c401bab96c85a034aa78313c8de83
Gideon Boakye <gboakye007@outlook.com>
2024-01-07 23:45:25 +0000

    small tailwind touches

--- 03tailwindandreactprops/src/components/Card.jsx
+++ 03tailwindandreactprops/src/components/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Card({username, position, imageSource}) {
   return (
-    <div>
-        <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <img className="w-24 h-24 rounded-full mx-auto" src={imageSource} alt="" width="384" height="512" />
+    <div >
+        <figure className="md:flex bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img className="w-24 h-24 md:object-contain md:w-48 md:h-48 md:rounded-none rounded-full mx-auto" src={imageSource} alt="" width="384" height="512" />
         <div className="pt-6 space-y-4">
             <blockquote>
             <p className="text-lg font-medium">
