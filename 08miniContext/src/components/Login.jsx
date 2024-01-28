1e7a02e17ac0678b0a7532021300fd185909e54d
Gideon Boakye <gboakye007@outlook.com>
2024-01-28 21:48:41 +0000

    personal portfolio

---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from 'react';
+import UserContext from '../context/UserContext';
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {setUser} = useContext(UserContext)
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setUser({username, password})
+    }
+
+  return (
+    <div>
+        <input
+        type='text'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        />
+        <input 
+        type="password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+        onClick={handleSubmit}
+        >
+            Submit
+        </button>
+    </div>
+  )
+}
+
+export default Login
